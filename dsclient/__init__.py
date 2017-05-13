3838f995c060946c61031b5fd4dfbc43e7b66b50
Yoichi Nagai <yoichi.nagai@gmail.com>
2017-05-13 11:08:38 +0900

    bug fix(change package name in distribution)

--- dsclient/__init__.py
+++ dsclient/__init__.py
@@ -2,4 +2,4 @@ from pkg_resources import get_distribution
 from . client import Client
 from . schema import Schema
 
-__version__ = get_distribution('gcp-dsclient').version
+__version__ = get_distribution('dsclient').version
