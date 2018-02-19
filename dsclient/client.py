6dad8f100a819ebd3a150dc14f8bcea34f6d3fb4
Yoichi Nagai <yoichi.nagai@gmail.com>
2018-02-19 21:55:12 +0900

    add extract_read_csv

--- dsclient/client.py
+++ dsclient/client.py
@@ -34,25 +34,40 @@ class Client(bigquery.Client, storage.Client, datastore.Client, compute.Client):
         gs_uri = "gs://{0}/{1}.csv".format(bucket, table_id)
         try:
             table = self.extract(table_name, gs_uri)
-        except:
-            if bucket == table_id:
-                self.delete_bucket(bucket)
-            self.delete_table(table_name)
+            s = schema.Schema(table["schema"])
+            dtype = s.get_object_dtype()
+            df = self.read_csv(gs_uri, dtype=dtype)
+            df = s.update_dtype(df)
+        finally:
             if dataset_id == table_id:
                 self.delete_dataset(dataset_id)
-            raise
+            else:
+                self.delete_table(table_name)
+            if bucket == table_id:
+                self.delete_bucket(bucket)
+            else:
+                self.delete_object(gs_uri)
 
-        self.delete_table(table_name)
-        if dataset_id == table_id:
-            self.delete_dataset(dataset_id)
+        return df
 
-        s = schema.Schema(table["schema"])
-        dtype = s.get_object_dtype()
-        df = self.read_csv(gs_uri, dtype=dtype)
-        df = s.update_dtype(df)
+    def extract_read_csv(self, table_name, bucket=None):
 
-        self.delete_object(gs_uri)
-        if bucket == table_id:
-            self.delete_bucket(bucket)
+        tmp_bucket = "tmp_{0}_{1}_{2}".format(os.uname()[1].replace("-","_"), os.getpid(), int(time.time()))
+
+        if bucket is None:
+            bucket = tmp_bucket
+            self.create_bucket(bucket)
+        gs_uri = "gs://{0}/{1}.csv".format(bucket, tmp_bucket)
+        try:
+            table = self.extract(table_name, gs_uri)
+            s = schema.Schema(table["schema"])
+            dtype = s.get_object_dtype()
+            df = self.read_csv(gs_uri, dtype=dtype)
+            df = s.update_dtype(df)
+        finally:
+            if bucket == tmp_bucket:
+                self.delete_bucket(bucket)
+            else:
+                self.delete_object(gs_uri)
 
         return df
